5379e1048f117663412d36034df5b9f230af3132
MrTurtles <mineshaftdave2@gmail.com>
2018-01-06 00:54:16 +0000

    Update setstatus.js

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   message.delete()
   let login = args.join(' ');
-  if(!login) login = 'm,info | I Like Turtles!';
+  if(!login) login = ',help | I Like Turtles!';
   client.user.setGame(login, 'https://www.twitch.tv/roblox');
 };
 
