87c886ef4f3b8e3f06507e513ea9ee86a4e3a251
MrTurtles <mineshaftdave2@gmail.com>
2018-01-13 18:21:00 +0100

    Add files via upload

--- commands/shoot.js
+++ commands/shoot.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message) => {
-  if (message.mentions.users.size < 1) return message.reply('@mention some people to shoot!').then(m => m.delete(5000));
+  message.delete();
+  if (message.mentions.users.size < 1) return message.channel.sendEmbed(new Discord.RichEmbed()
+            .setColor(0x00E90B0B)
+            .setTimestamp()
+            .addField(`Error ❌`, `Sorry, I can't shoot ghosts. :wink:`)).then(m => m.delete(5000)).catch(console.error);
 
     const embed2 = new Discord.RichEmbed()
     .setColor(0x11B8D6)
     .setTimestamp()
-    .addField(`${message.author.username} killed:`, `${message.mentions.users.map(m => `**${m}** :gun:`).join('\n')}`)
-
-    message.delete();
+    .addField(`${message.author.username} killed:`, `${message.mentions.users.map(m => `**${m}** :gun:`).join('\n')}`);
     message.channel.sendEmbed(embed2);
 };
 
