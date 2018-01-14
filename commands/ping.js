a7f7984036e8ba66b1570f218653ce5caa9ad573
MrTurtles <mineshaftdave2@gmail.com>
2018-01-14 20:56:13 +0100

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,13 @@
 exports.run = (client, message) => {
   message.delete();
-  message.channel.sendMessage('Ping?')
+  /*message.channel.sendMessage('Ping?')
     .then(msg => {
       msg.edit(`Pong! (took: ${msg.createdTimestamp - message.createdTimestamp}ms)`).then(m => m.delete(10000));
-    });
+    });*/
+  message.channel.sendEmbed(
+      new Discord.RichEmbed()
+      .setColor(0x00E90B0B)
+      .addField(`PONG!`, `${client.ping}ms`));
 };
 
 exports.conf = {
