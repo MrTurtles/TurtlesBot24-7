fedaeac9ce7a49b2b603bab92c890c295a2e8e49
MrTurtles <mineshaftdave2@gmail.com>
2018-01-14 20:42:49 +0100

    Add files via upload

--- commands/eval.js
+++ commands/eval.js
@@ -2,20 +2,31 @@ const Discord = require('discord.js');
 //const settings = require('../settings.json');
 exports.run = async (client, message, args) => {
     message.delete();
-    let ownerid = 275303108589125633;
-    if(message.author.id !== ownerid) return;
+    let ownerID = 275303108589125633;
+    /*if(message.author.id !== ownerID) return;
     const code = args.join(" ");
     try {
       const evaled = client.clean(await eval(code));
       if(message.flags.includes("d")) message.delete();
       if(message.flags.includes("s")) return;
       message.channel.send(`\`\`\`xl\n${evaled}\n\`\`\``);
-    }
-    catch(err) {
+    } catch(err) {
       if(message.flags[0] && message.flags[0] === 's')
         return message.delete();
       message.channel.send(`\`ERROR\` \`\`\`xl\n${client.clean(err)}\n\`\`\``);
-   }
+    }*/
+    if(message.author.id !== ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
 };
 
 exports.conf = {
