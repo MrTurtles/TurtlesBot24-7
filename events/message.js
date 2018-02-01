e850ba0d9ce944853350e59fb555904bc9a60348
MrTurtles <mineshaftdave2@gmail.com>
2018-02-01 18:22:18 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -36,7 +36,12 @@ module.exports = message => {
     .setTimestamp()
     .addField(`Error :no_entry:`, `${message.author}, This command is from another bot and cannot be used without the bot in the server.`)
   if (message.content.startsWith(`gn`)) return message.react('🙋' && `😘`);
-  if (message.content.startsWith(`;paycode`)) return message.delete() && message.channel.sendEmbed(embed6234).then(m => m.delete(8000));
+  if (message.content.startsWith(`;paycode`)) {
+    message.delete(); 
+    message.channel.sendEmbed(embed6234).then(m => m.delete(8000));
+    console.log(message);
+    return;
+  };
   if (message.content.startsWith(`<@304258486466904064>`)) return message.react('🎉');
   if (!message.content.startsWith(prefix)) return;
   if (message.channel.type == 'dm') return message.channel.sendEmbed(embed66);
