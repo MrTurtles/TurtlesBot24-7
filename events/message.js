7f67396f9ecacf49cead9f66cc1ac520a1d4559c
MrTurtles <mineshaftdave2@gmail.com>
2018-03-02 01:18:30 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,7 +5,6 @@ module.exports = message => {
   let client = message.client;
   let prefix = ",";
   if (message.author.bot) return;
-  let blacklist = ["368400509087907843","329262085445779456"]
 
   //if (sales.login = `,help | Off Sale!`) {
   //  let sales2 = `Off!`;
@@ -51,7 +50,10 @@ module.exports = message => {
   if (!message.content.startsWith(prefix)) return;
   if (message.channel.type == 'dm') return message.channel.sendEmbed(embed66);
   if (message.channel.type == 'group') return message.channel.sendEmbed(embed66);
-  if (message.author.id === "368400509087907843" || "368400509087907843") return message.channel.sendEmbed(embed5729).then(m => m.delete(5000));
+  if (message.author.id == "368400509087907843" || "368400509087907843") return {
+    message.delete();
+    message.channel.sendEmbed(embed5729).then(m => m.delete(5000));
+  }
   let command = message.content.split(' ')[0].slice(prefix.length);
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
