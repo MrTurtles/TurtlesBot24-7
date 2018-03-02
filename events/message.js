42a130e9d2ef973d3603a99bcde7858a6628e544
MrTurtles <mineshaftdave2@gmail.com>
2018-03-02 01:23:43 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -50,9 +50,10 @@ module.exports = message => {
   if (!message.content.startsWith(prefix)) return;
   if (message.channel.type == 'dm') return message.channel.sendEmbed(embed66);
   if (message.channel.type == 'group') return message.channel.sendEmbed(embed66);
-  if (message.author.id === '368400509087907843') return {
+  if (message.author.id === '368400509087907843') {
     message.delete()
-    message.channel.sendEmbed(embed5729).then(m => m.delete(5000));
+    message.channel.sendEmbed(embed5729).then(m => m.delete(5000))
+    return;
   }
   let command = message.content.split(' ')[0].slice(prefix.length);
   let params = message.content.split(' ').slice(1);
