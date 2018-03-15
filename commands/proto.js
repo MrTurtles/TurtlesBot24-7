4ca39c2f035a01113eb749a1ba9a09c61eff4232
MrTurtles <mineshaftdave2@gmail.com>
2018-03-15 22:16:10 +0100

    Update proto.js

--- commands/proto.js
+++ commands/proto.js
@@ -1,15 +1,30 @@
 const Discord = require('discord.js');
 exports.run = (client, message) => {
-  message.channel.sendEmbed(
-      new Discord.RichEmbed()
-      .setColor(0x11B8D6)
-      .setAuthor("Protosmasher Information", "https://i.imgur.com/lw8NlD2.png")
-      .setDescription("Info about updates and stuff.")
-      .setThumbnail("https://i.imgur.com/lw8NlD2.png")
-      .addBlankField(true)
-      .addBlankField(true)
-      .addField(`Updated?`, `NO`, true)
-      .addField(`Issues?`, `-`, true));
+    let user = message.author;
+    let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
+    if (!muteRole) return message.channe;
+    if (message.channel.id == "295877326762278912") {
+        message.guild.member(user).addRole(muteRole).then(() => {
+      message.channel.sendEmbed(new Discord.RichEmbed()
+            .setColor(0x0013CF0E)
+            .setTimestamp()
+            .addField(`Auto-Mute ✅`, `Succesfully muted **${user}** for **30** Minutes.\n**Reason:** Use fking #bot-commands`));
+       setTimeout(function() {
+        message.guild.member(user).removeRole(muteRole);
+       }, 30*60000); 
+      }); 
+    } else {
+        message.channel.sendEmbed(
+       new Discord.RichEmbed()
+       .setColor(0x11B8D6)
+       .setAuthor("Protosmasher Information", "https://i.imgur.com/lw8NlD2.png")
+       .setDescription("Info about updates and stuff.")
+       .setThumbnail("https://i.imgur.com/lw8NlD2.png")
+       .addBlankField(true)
+       .addBlankField(true)
+       .addField(`Updated?`, `NO`, true)
+       .addField(`Issues?`, `-`, true));
+    }
 };
 
 exports.conf = {
