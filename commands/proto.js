ce2bf21751458440e2b559e5f66f8852a03e37d2
MrTurtles <mineshaftdave2@gmail.com>
2018-03-23 06:36:32 +0100

    Update proto.js

--- commands/proto.js
+++ commands/proto.js
@@ -25,7 +25,7 @@ exports.run = (client, message) => {
        .addBlankField(true)
        .addField(`:arrows_counterclockwise: Update Status:`, `UPDATED!`, true)
        .addField(`:warning: Issues with?`, `Execution + Injection`, true)
-       .addField(`:arrows_counterclockwise: Website Status:`, `DOWN`, true));
+       .addField(`:arrows_counterclockwise: Website Status:`, `ONLINE`, true));
     }
 };
 
