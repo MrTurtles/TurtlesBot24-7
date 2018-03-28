3c11c3bad9fd50c0be71b4b01802f6f7c67ea446
MrTurtles <mineshaftdave2@gmail.com>
2018-03-28 18:19:50 +0200

    Update proto.js

--- commands/proto.js
+++ commands/proto.js
@@ -23,8 +23,8 @@ exports.run = (client, message) => {
        .setThumbnail("https://i.imgur.com/lw8NlD2.png")
        .addBlankField(true)
        .addBlankField(true)
-       .addField(`:arrows_counterclockwise: Update Status:`, `UPDATED!`, true)
-       .addField(`:warning: Issues with?`, `Execution + Injection`, true)
+       .addField(`:arrows_counterclockwise: Update Status:`, `NOT UPDATED!`, true)
+       .addField(`:warning: Issues with?`, `-`, true)
        .addField(`:arrows_counterclockwise: Website Status:`, `ONLINE`, true));
     }
 };
