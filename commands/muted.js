928715509d49fc8261af5f8ae795f94a7758afd5
MrTurtles <mineshaftdave2@gmail.com>
2018-04-01 14:22:35 +0200

    Update muted.js

--- commands/muted.js
+++ commands/muted.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message) => {
+  message.delete();
   let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
   if(!muteRole) return message.channel.sendEmbed(new Discord.RichEmbed()
             .setColor(0x00E90B0B)
@@ -10,7 +11,7 @@ exports.run = (client, message) => {
             .setTitle('Muted users:')
             .setColor(0x11B8D6)
             .setDescription(message.guild.roles.get(muteRole.id).members.map(m=>m.user.tag).join('\n\n'));
-  message.channel.send(ListEmbed);  
+  message.channel.send(ListEmbed).then(m => m.delete(15000));  
 };
 
 exports.conf = {
