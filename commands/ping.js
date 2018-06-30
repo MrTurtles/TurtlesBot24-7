25ddab854420c9de6b447fc1eb7f3d1c0adb86cf
MrTurtles <mineshaftdave2@gmail.com>
2018-06-30 12:23:37 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 exports.run = (client, message) => {
   message.delete();
+  let modlog = message.guild.channels.find('name', 'logs');
+  const embed = new Discord.RichEmbed()
+    .setColor(0x11B8D6)
+    .setTimestamp()
+    .addField('Action', 'Test')
   /*message.channel.sendMessage('Ping?')
     .then(msg => {
       msg.edit(`Pong! (took: ${msg.createdTimestamp - message.createdTimestamp}ms)`).then(m => m.delete(10000));
     });*/
+  client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
   message.channel.sendEmbed(
       new Discord.RichEmbed()
       .setColor(0x11B8D6)
